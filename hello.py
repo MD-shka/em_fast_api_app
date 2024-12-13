8d10594a1e30abe0fdbccce069c142f96800a8fd
Mikhail Egorov <pe.egorov@gmail.com>
2024-12-13 18:14:31 +0500

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""
+This is a simple Python script that prints a message to the console.
+"""
+
+
+def main():
+    """
+    Main function of the app.
+    """
+    print("Hello from em-fast-api-app!")
+
+
+if __name__ == "__main__":
+    main()
